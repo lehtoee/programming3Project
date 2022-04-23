dbd0e55ebe0a04e3f45692fad452f76ed1e37d52
Eemil Lehto <eemil.lehto@tuni.fi>
2022-04-23 15:17:22 +0300

    Add correct project structure

---
+++ Sisu_FXML/src/main/java/fi/tuni/prog3/sisu_fxml/SecondaryController.java
@@ -0,0 +1,12 @@
+package fi.tuni.prog3.sisu_fxml;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
---
+++ Sisu_FXML/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module fi.tuni.prog3.sisu_fxml {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens fi.tuni.prog3.sisu_fxml to javafx.fxml;
+    exports fi.tuni.prog3.sisu_fxml;
+}
